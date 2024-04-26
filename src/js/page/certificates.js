67c3af64808e521eb64abfc9a989bebf0cb5928d
MonstersBY <monsters.alex0@gmail.com>
2024-04-26 12:39:51 +0300

    feat: sertificate

---
+++ src/js/page/certificates.js
@@ -0,0 +1,20 @@
+import $ from "jquery";
+import Swiper from 'swiper';
+import {
+	Pagination,
+	Navigation,
+	Grid
+} from "swiper/modules";
+import { rem } from '../utils/constants';
+
+if (window.screen.width < 769) {
+	const swiper_injections_swiper = new Swiper('.certificates-info_swiper', {
+		modules: [Pagination],
+		slidesPerView: 1,
+		spaceBetween: rem(1.2),
+        pagination: {
+            el: ".certificates-info_pagination",
+        },
+	});
+}
+
